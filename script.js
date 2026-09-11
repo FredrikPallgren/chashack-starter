d6670c505baf07b6cf432fcefd52681e95534e15
Andrija <andrijadjokic07@gmail.com>
2026-09-11 13:10:00 +0200

    commit

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let nummbercounter = document.getElementById("nummberCounter");
 let buttonCounter = document.getElementById("ButtonCounter");
+const stopit = document.getElementById("stopit");
+let spammessage = document.getElementById("spammessage");
+const spam = 10;
 
 
 
@@ -7,5 +10,10 @@ buttonCounter.addEventListener("click", function(){
  let currentValue = nummbercounter.innerHTML;
  currentValue++;
  nummbercounter.innerHTML = currentValue;
+
+ if (currentValue >= spam){
+    stopit.style.display = "block";
+    spammessage.innerHTML = "Please stop it already!";
+}
 })
 ;
