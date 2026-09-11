5ec036b2940d3a131e87c828d1e5c692c14d16ba
Andrija <andrijadjokic07@gmail.com>
2026-09-11 11:03:24 +0200

    Jag har lagt till button element i index.html filen, tillsammans med texten. Javascript förendrar text content varje gång man trycker på button element

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let nummbercounter = document.getElementById("nummberCounter");
+let buttonCounter = document.getElementById("ButtonCounter");
 
+
+
+buttonCounter.addEventListener("click", function(){
+ let currentValue = nummbercounter.innerHTML;
+ currentValue++;
+ nummbercounter.innerHTML = currentValue;
+})
+
+;
